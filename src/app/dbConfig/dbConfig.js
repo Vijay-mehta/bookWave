de9081b58a42ce5a37d55f5dd552e41713838a28
Vijay-mehta <infovijay1998@gmail.com>
2024-06-04 07:56:20 +0530

    changes in mongoConnect function

--- src/app/dbConfig/dbConfig.js
+++ src/app/dbConfig/dbConfig.js
@@ -1,17 +1,27 @@
 import mongoose from "mongoose";
 
 export async function mongoConnect() {
-  try {
- mongoose.connect(process.env.MONGO_URL)
- const connection =mongoose.connection;
- connection.on('connected',()=>{
-    console.log("MOnogoDB connected successfully")
- })
- connection.on('erre',(error)=>{
-    console.log("MongoDB connection error Please make sure MongoDB is runing."+error)
- })
-  } catch (error) {
-    console.log("Something goes wrong!")
-    console.log(error)
+  if (mongoose.connection.readyState >= 1) {
+    console.log("MongoDB is already connected.");
+    return;
   }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(
+      "MongoDB connection error. Please make sure MongoDB is running.",
+      error
+    );
+  }
+
+  mongoose.connection.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.warn("MongoDB connection lost. Attempting to reconnect...");
+    mongoConnect();
+  });
 }
